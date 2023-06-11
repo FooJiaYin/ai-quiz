8950bed38bd2692a708d9ea6ce0803bf501b52b8
FooJiaYin <candy23198@hotmail.com>
2023-06-11 11:20:23 +0800

    UI: Change theme color

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -3,10 +3,33 @@ import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import { defineNuxtPlugin } from "nuxt/app";
 
+const theme = {
+    dark: false,
+    colors: {
+        background: '#121212',
+        surface: '#FFFFFF',
+        primary: '#3F51B5',
+        'primary-darken-1': '#303F9F',
+        secondary: '#03DAC6',
+        'secondary-darken-1': '#018786',
+        error: '#F44336',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FB8C00',
+    },
+};
+
 export default defineNuxtPlugin((nuxtApp) => {
     const vuetify = createVuetify({
         ssr: true,
         treeShake: true,
+        theme: {
+            // defaultTheme: 'dark',
+            defaultTheme: 'theme',
+            themes: {
+                theme,
+            }
+        }
     });
 
     nuxtApp.vueApp.use(vuetify);
