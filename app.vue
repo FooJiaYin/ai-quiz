95e9e876a5ac71d3014a8db365ed7be582de9dab
FooJiaYin <candy23198@hotmail.com>
2023-06-11 11:18:23 +0800

    feat: Navigation drawer

--- app.vue
+++ app.vue
@@ -1,3 +1,10 @@
 <template>
-	<NuxtPage />
+	<v-layout>
+		<NavDrawer />
+		<v-main>
+			<v-container class="w-75">
+				<NuxtPage />
+			</v-container>
+		</v-main>
+	</v-layout>
 </template>
---
+++ components/nav/Drawer.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-navigation-drawer class="bg-indigo-darken-1" permanent theme="dark">
+        <v-list nav>
+            <NavLink link="/" title="+ Add Quiz"></NavLink>
+            <NavLink link="/main-points" title="Main Points"></NavLink>
+            <NavLink link="/single-choice" title="Single Choice Quiz"></NavLink>
+        </v-list>
+    </v-navigation-drawer>
+</template>
+
+<style>
+.v-navigation-drawer,
+.v-navigation-drawer * {
+    transition-duration: 0s !important;
+}
+</style>
