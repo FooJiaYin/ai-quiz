1fa0a754e6045ac9ac02f7060e0a4a85595567a5
FooJiaYin <candy23198@hotmail.com>
2023-06-11 14:56:32 +0800

    feat: Example store action to call api

--- composables/useStore.js
+++ composables/useStore.js
@@ -1,7 +1,19 @@
 import { defineStore } from 'pinia';
 
 export const useStore = defineStore('quiz', {
-  state: () => {
-    return {};
-  },
+    state: () => {
+        return {};
+    },
+	actions: {
+		async getOpenAIResponse(transcript) {
+			try {
+				const response = await $fetch('/api/chat', { 
+					method: 'POST', body: JSON.stringify({ n: 2, prompt: transcript }) 
+				})
+				return response;
+			} catch (error) {
+				console.error(error);
+			}
+		},
+	},
 });
