e4afd41f5b411b83469141e26d146ff54624ad0a
FooJiaYin <candy23198@hotmail.com>
2023-06-11 12:50:46 +0800

    style: Change tab size

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,16 +2,16 @@
 import vuetify from 'vite-plugin-vuetify'
 
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  build: {
-    transpile: ['vuetify'],
-  },
-  modules: [
-    '@pinia/nuxt',
-    async (options, nuxt) => {
-      nuxt.hooks.hook('vite:extendConfig', config => config.plugins.push(
-        vuetify()
-      ))
-    },
-  ],
+	devtools: { enabled: true },
+	build: {
+		transpile: ['vuetify'],
+	},
+	modules: [
+		'@pinia/nuxt',
+		async (options, nuxt) => {
+			nuxt.hooks.hook('vite:extendConfig', config => config.plugins.push(
+				vuetify()
+			))
+		},
+	],
 })
