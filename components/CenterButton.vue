e25752474dd0525988d2a936e2cea37bea5d09fd
FooJiaYin <candy23198@hotmail.com>
2023-06-11 11:15:53 +0800

    feat: Input page UI

---
+++ components/CenterButton.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="d-flex">
+        <v-btn color="primary" class="mx-auto" @click="$emit('click')">
+            <slot></slot>
+        </v-btn>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,5 @@
 <template>
-    <NuxtWelcome />
+    <h3 class="mb-4" ref="form">Input transcript</h3>
+    <v-textarea label="Insert trancript here" variant="outlined" clearable></v-textarea>
+    <CenterButton>Generate Quiz</CenterButton>
 </template>
