e3be4c4f31974bbf92c746998c35a0f361f2d675
FooJiaYin <candy23198@hotmail.com>
2023-06-11 20:27:32 +0800

    feat(main-points): Render markdown

---
+++ components/markdown.vue
@@ -0,0 +1,11 @@
+<template>
+    <div v-html="html"></div>
+</template>
+
+<script setup>
+import showdown from "showdown";
+const props = defineProps(['src']);
+const converter = new showdown.Converter();
+const html = ref("<p></p>");
+html.value = converter.makeHtml(props.src);
+</script>
--- pages/main-points.vue
+++ pages/main-points.vue
@@ -1,7 +1,13 @@
 <template>
     <h2 class="mt-8 mb-4">What will you learn:</h2>
-    <pre>{{ quiz.mainPoints }}</pre>
+    <Markdown :src="quiz.mainPoints"></Markdown>
 </template>
+
+<style>
+li {
+    margin-bottom: 1em;
+}
+</style>
 
 <script setup>
 const quiz = useQuiz();
