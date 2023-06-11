81d3f5c2afc08ff84593e92a37a626a9dd067e53
FooJiaYin <candy23198@hotmail.com>
2023-06-11 14:56:54 +0800

    feat: Add demo input page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,16 @@
 <template>
-    <NuxtWelcome />
+    <v-textarea label="Insert prompt here" v-model="input" variant="outlined" clearable></v-textarea>
+    <v-btn @click="getResponse">Get Response</v-btn>
+    <p>{{ response }}</p>
 </template>
+
+<script setup>
+const store = useStore();
+const input = ref("");
+const response = ref("");
+
+async function getResponse() {
+    const res = await store.getOpenAIResponse(input.value);
+    response.value = res;
+}
+</script>
