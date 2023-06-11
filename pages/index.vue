c76076646444ae82a6746b94630e19633469d19c
FooJiaYin <candy23198@hotmail.com>
2023-06-11 11:32:54 +0800

    feat: Generate quiz action

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
 <template>
     <h3 class="mb-4" ref="form">Input transcript</h3>
-    <v-textarea label="Insert trancript here" variant="outlined" clearable></v-textarea>
-    <CenterButton>Generate Quiz</CenterButton>
+    <v-textarea label="Insert trancript here" v-model="input" variant="outlined" auto-grow clearable></v-textarea>
+    <CenterButton @click="generateQuiz">Generate Quiz</CenterButton>
 </template>
+
+<script setup>
+const quiz = useQuiz();
+const input = ref("");
+
+function generateQuiz() {
+    quiz.generateQuiz(input.value);
+}
+</script>
