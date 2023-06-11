fdaf1256c169ad58ea7f5e4606da1175cd8d41dd
FooJiaYin <candy23198@hotmail.com>
2023-06-11 19:10:31 +0800

    feat(input): Add language field

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,14 @@
 <template>
     <h3 class="mb-4" ref="form">Input transcript</h3>
     <v-textarea label="Insert trancript here" v-model="input" variant="outlined" auto-grow clearable></v-textarea>
+    <v-text-field label="Language" v-model="language" variant="outlined"></v-text-field>
     <CenterButton @click="generateQuiz">Generate Quiz</CenterButton>
 </template>
 
 <script setup>
 const quiz = useQuiz();
 const input = ref("");
-const language = ref("en-us");
+const language = ref("");
 
 async function generateQuiz() {
     await quiz.generateQuiz(input.value, language.value);
