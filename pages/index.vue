d30ecd136ca164ebe83a125feaf2cbe08e1fda16
FooJiaYin <candy23198@hotmail.com>
2023-06-11 19:12:08 +0800

    feat(input): Show loading status

--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,18 @@
     <v-textarea label="Insert trancript here" v-model="input" variant="outlined" auto-grow clearable></v-textarea>
     <v-text-field label="Language" v-model="language" variant="outlined"></v-text-field>
     <CenterButton @click="generateQuiz">Generate Quiz</CenterButton>
+    <p>{{ status }}</p>
 </template>
 
 <script setup>
 const quiz = useQuiz();
 const input = ref("");
 const language = ref("");
+const status = ref("");
 
 async function generateQuiz() {
+    status.value = "Loading...";
     await quiz.generateQuiz(input.value, language.value);
+    status.value = "Complete!";
 }
 </script>
