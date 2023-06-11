91064744b39b42ddca2a6b84e8030cbc2472f603
FooJiaYin <candy23198@hotmail.com>
2023-06-11 19:27:21 +0800

    feat(main-points): Show main points

--- pages/main-points.vue
+++ pages/main-points.vue
@@ -1,1 +1,8 @@
-<template></template>
+<template>
+    <h2 class="mt-8 mb-4">What will you learn:</h2>
+    <pre>{{ quiz.mainPoints }}</pre>
+</template>
+
+<script setup>
+const quiz = useQuiz();
+</script>
