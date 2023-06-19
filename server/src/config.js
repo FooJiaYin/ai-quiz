dbf08c72325e1301648c4f5a5244f35ddd4510b3
FooJiaYin <candy23198@hotmail.com>
2023-06-19 08:13:52 +0800

    feat: Switch to gpt-3.5-turbo-0613

--- server/src/config.js
+++ server/src/config.js
@@ -1,5 +1,6 @@
 const defaultParams = {
-    model: "gpt-3.5-turbo",
+    // TODO: `gpt-3.5-turbo-0613` will be deprecated 3 months after `gpt-3.5-turbo-0301` is replaced
+    model: "gpt-3.5-turbo-0613",
     messages: [],
     temperature: 0.0,
     max_tokens: 1000,
