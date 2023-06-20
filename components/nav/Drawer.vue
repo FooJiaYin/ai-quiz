17e974f452fcbcd77144294f74f86cab6b762e8d
FooJiaYin <candy23198@hotmail.com>
2023-06-20 19:38:28 +0800

    style: Rename mainpoint page

--- components/nav/Drawer.vue
+++ components/nav/Drawer.vue
@@ -2,7 +2,7 @@
     <v-navigation-drawer class="bg-indigo-darken-1" permanent theme="dark">
         <v-list nav>
             <NavLink link="/" title="+ Add Quiz"></NavLink>
-            <NavLink v-if="quiz['mainpoints'].length > 0" link="/main-points" title="Main Points"></NavLink>
+            <NavLink v-if="quiz['mainpoints'].length > 0" link="/mainpoints" title="Main Points"></NavLink>
             <NavLink v-if="quiz['MC'].length > 0" link="/multiple-choice" title="Multiple Choice Quiz"></NavLink>
             <NavLink v-if="quiz['TF'].length > 0" link="/true-false" title="True/False Quiz"></NavLink>
         </v-list>
