010b5e8f9c1c32a2a7a922d08590b29c6254e1f9
FooJiaYin <candy23198@hotmail.com>
2023-06-20 22:28:50 +0800

    refactor(components): Draggable component

---
+++ components/Draggable.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="text-align-center bg-primary rounded-lg px-4 py-2" 
+        draggable="true" @dragstart="drag.set(keyword)">
+        {{ keyword }}
+    </div>
+</template>
+
+<script setup>
+const props = defineProps(['keyword']);
+const drag = useDrag();
+</script>
--- components/DraggableKeywords.vue
+++ components/DraggableKeywords.vue
@@ -2,10 +2,7 @@
 <template>
     <v-row class="my-6">
         <v-col v-for="(keyword, index) in shuffledKeywords" cols="auto" class="py-1" :key="index">
-            <div class="text-align-center bg-primary rounded-lg px-4 py-2" 
-                draggable="true" @dragstart="drag.set(keyword)">
-                {{ keyword }}
-            </div>
+            <Draggable :keyword="keyword" />
         </v-col>
     </v-row>
 </template>
