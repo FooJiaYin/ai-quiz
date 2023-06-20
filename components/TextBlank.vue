72d457e5a5a9da4c43c816bf7bc3cb60d32caa88
FooJiaYin <candy23198@hotmail.com>
2023-06-20 22:16:25 +0800

    UI: TextBlank style

--- components/TextBlank.vue
+++ components/TextBlank.vue
@@ -1,9 +1,17 @@
 <template>
-    <v-text-field variant="outlined" readonly
+    <v-text-field variant="outlined" readonly hide-details
         :class="value !== '' ? answer === value ? 'text-success' : 'text-error' : ''" 
         v-model="value" @drop="value = drag.text" @dragover.prevent>
     </v-text-field>
 </template>
+
+<style scoped>
+.v-text-field >>> input {
+    padding: 5px !important;
+    min-height: 1em;
+    text-align: center;
+}
+</style>
 
 <script setup>
 const props = defineProps(['answer']);
