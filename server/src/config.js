d8a69b2c966c82e46287b17cb5dcd86b217f13e3
FooJiaYin <candy23198@hotmail.com>
2023-07-11 14:14:55 +0800

    chore: Update model

--- server/src/config.js
+++ server/src/config.js
@@ -1,6 +1,5 @@
 const defaultParams = {
-    // TODO: `gpt-3.5-turbo-0613` will be deprecated 3 months after `gpt-3.5-turbo-0301` is replaced
-    model: "gpt-3.5-turbo-0613",
+    model: "gpt-3.5-turbo",
     messages: [],
     temperature: 0.0,
     max_tokens: 1000,
